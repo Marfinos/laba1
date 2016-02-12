5b05b79e6b56d4fc237bd71a7f24aeac623dde35
Marfinos <lexonbag@mail.ru>
2016-02-12 14:08:32 +0300

    add main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a,b;
+int main()
+{
+    printf("Vvedite chislo A \n");
+    scanf("%d",&a);
+    printf("Vvedite chislo B \n");
+    scanf("%d",&b);
+    printf("%d + %d = %d", a,b,a+b);
+
+    return 0;
+}
