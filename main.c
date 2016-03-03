54bed58d39c8bbee4b7edacc43a718e42f9d3c0f
Marfinos <lexonbag@mail.ru>
2016-03-03 13:40:39 +0300

    add proizvedenie

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ int main()
     printf("Vvedite chislo B \n");
     scanf("%d",&b);
     printf("%d + %d = %d \n", a,b,a+b);
-    printf("%d - %d = %d", a,b,a-b);
+    printf("%d - %d = %d \n", a,b,a-b);
+    printf("%d * %d = %d \n", a,b,a*b);
 
     return 0;
 }
